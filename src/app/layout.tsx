20a2d68cefcb9218ff56441b0355ef184c1b704d
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-07-28 17:08:32 +0530

    🐛 FIX:  meta tag title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,23 +3,23 @@ import './global.scss';
 // import { Inter } from 'next/font/google';
 
 // export const metadata: Metadata = {
-//     title: 'Project X',
-//     description: 'Welcome to Project X - Biggest event is Sushant lok',
+//     title: 'Project Sushant Lok',
+//     description: 'Welcome to Project Sushant Lok - Biggest event is Sushant lok',
 // };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <head>
-                <title>Project X - The Biggest Party in Sushant Lok</title>
-                <meta name="title" content="Project X - The Biggest Party in Sushant Lok" />
+                <title>Project Sushant Lok - The Biggest Party in Sushant Lok</title>
+                <meta name="title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
                 <meta
                     name="description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
                 />
 
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="Project X - The Biggest Party in Sushant Lok" />
+                <meta property="og:title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
                 <meta
                     property="og:description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta property="og:image" content="/janeesh.jpg" />
 
                 <meta property="twitter:card" content="/janeesh.jpg" />
-                <meta property="twitter:title" content="Project X - The Biggest Party in Sushant Lok" />
+                <meta property="twitter:title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
                 <meta
                     property="twitter:description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
