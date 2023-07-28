1296c1a92f0a2eb5b9c9d8a2ffd6f7d21af5a846
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-07-28 17:36:56 +0530

    👌 IMPROVE:  meta tags description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,14 +12,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <head>
                 <title>Project Sushant Lok - The Biggest Party in Sushant Lok</title>
-                <meta name="title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
+                <meta name="title" content="Project Sushant Lok - Be a part of the greatness" />
                 <meta
                     name="description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
                 />
 
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
+                <meta property="og:title" content="Project Sushant Lok - Be a part of the greatness" />
                 <meta
                     property="og:description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta property="og:image" content="/janeesh.jpg" />
 
                 <meta property="twitter:card" content="/janeesh.jpg" />
-                <meta property="twitter:title" content="Project Sushant Lok - The Biggest Party in Sushant Lok" />
+                <meta property="twitter:title" content="Project Sushant Lok - Be a part of the greatness" />
                 <meta
                     property="twitter:description"
                     content="C318, Block C, Sushant Lok phase 1, near Fauji Market, Sector 43, Gurgaon."
