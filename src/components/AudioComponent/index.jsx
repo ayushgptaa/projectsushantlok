ab5833d19a4d6b69c27a7bd39f0cc82c22f28047
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-07-28 20:19:23 +0530

    🐛 FIX:  AudioComponent

--- src/components/AudioComponent/index.jsx
+++ src/components/AudioComponent/index.jsx
@@ -4,12 +4,10 @@ import { useState, useRef } from 'react';
 
 const AudioComponent = () => {
     const audioRef = useRef(null);
-    const buttonRef = useRef(null);
 
     const [isPlaying, setIsPlaying] = useState(false);
 
     const handleTogglePlayPause = () => {
-        console.log('called');
         if (audioRef.current.paused) {
             audioRef.current.play();
         } else {
@@ -22,8 +20,7 @@ const AudioComponent = () => {
             <audio autoPlay ref={audioRef} src="/themeSong.mp3" />
             <button
                 style={{ padding: '5px 10px', position: 'absolute', top: '20px', right: '20px' }}
-                ref={buttonRef}
-                onClick={() => handleTogglePlayPause()}>
+                onClick={handleTogglePlayPause}>
                 {isPlaying ? 'Pause Music' : 'Play music'}
             </button>
         </>
