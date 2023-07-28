c3bd409832035c7375830f659027e16f1b41b24d
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-07-28 13:56:17 +0530

    🐛 FIX:  auth

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,6 +1,6 @@
 export function authenticate(password: string) {
     if (password === '$3NDNUD35') {
-        typeof window !== 'undefined' && localStorage.setItem('password', JSON.stringify({ password }));
+        typeof window !== 'undefined' && localStorage.setItem('password', password);
         return true;
     }
     return false;
